dc5500379d75c83680177f3dd2eb98cf3e36e1b7
GuilhermeBrandaoo <desenvolvedorguilhermebrandao@gmail.com>
2025-11-04 20:17:59 -0300

    commit

---
+++ desafio-6/script.js
@@ -0,0 +1,7 @@
+let numero = Number(prompt("Digite um número: "))
+let soma
+
+for (i = 0; i <= numero; i++) {
+    soma = i + numero;
+    console.log(numero + " + " + i + " = " + soma)
+} 
---
+++ desafio-7/script.js
@@ -0,0 +1,7 @@
+let numero = Number(prompt("Digite um número: "))
+let resultado
+
+for (i = 0; i <= 10; i++) {
+    resultado = numero * i
+    console.log(numero + " * " + i + " = " + resultado)
+}
